e3a65d5a1999ae1d7fdb725165c6da81361d3abc
foxtwobao <hulei.cn@gmail.com>
2026-06-27 14:30:26 +0800

    test: accept windows active session usernames

--- tests/test_windows_sessions.py
+++ tests/test_windows_sessions.py
@@ -1,10 +1,18 @@
 import getpass
+import sys
 
 from src.windows_sessions import current_interactive_username, select_interactive_username
 
 
-def test_current_interactive_username_falls_back_off_windows():
-    assert current_interactive_username() == getpass.getuser()
+def test_current_interactive_username_falls_back_off_windows_or_returns_active_windows_user():
+    username = current_interactive_username()
+    local_user = getpass.getuser()
+
+    if sys.platform == "win32":
+        assert username
+        assert username.split("\\")[-1] == local_user
+    else:
+        assert username == local_user
 
 
 def test_select_interactive_username_prefers_active_session_user():
